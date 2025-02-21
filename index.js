5e9b94d2482ea8066830df6e9a7d492d4dd9f110
Hayder Ali <hayderbd4290@gmail.com>
2025-02-21 11:35:04 +0600

    successfully connect on mongodb and task post api created

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-
 require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 const app = express();
 
+app.use(cors());
+app.use(express());
 
-app.use(cors())
-app.use(express())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bixye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const taskDb = client.db("Task-Management");
+    const taskCollection = taskDb.collection("taskCollection");
+
+    //    set task database
+    app.post("/task", async (req, res) => {
+      try {
+        const email = query.email;
+        if (!email) {
+          return res.send({ status: "You Need Login First" });
+        }
+        const task = req.body;
+        const result = await taskCollection.insertOne(task);
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
 
 
+    
+  } finally {
+    console.log("mongodb running");
+  }
+}
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Task Management Server Running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('Task Management Server Running')
-})
-
-app.listen(port, ()=>{
-    console.log(`server Running At : ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server Running At : ${port}`);
+});
